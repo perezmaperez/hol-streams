0b8aef3ebb8c466b2ab43dde3085c328428ab5b1
michaelperez <michael.perez@yahoo.com>
2022-06-11 06:19:59 -0500

    Completed solutions: Unit1, Unit2.

--- src/main/java/com/speedment/example/solution/Unit1MyCreate.java
+++ src/main/java/com/speedment/example/solution/Unit1MyCreate.java
@@ -3,7 +3,11 @@ package com.speedment.example.solution;
 import com.speedment.example.unit.Unit1Create;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
@@ -12,41 +16,47 @@ public final class Unit1MyCreate implements Unit1Create {
 
     @Override
     public Stream<String> newStreamOfAToC() {
-        return Stream.empty();
+        List<String> aToCList = Arrays.asList("A","B","C");
+        return aToCList.stream();
     }
 
     @Override
     public IntStream newIntStreamOfOneToSeven() {
-        return IntStream.empty();
+        return IntStream.of(1,2,3,4,5,6,7);
     }
 
     @Override
     public Stream<String> from(String[] array) {
-        return Stream.empty();
+        String[] texts = {"Alpha", "Bravo", "Charlie"};
+        return Stream.of(texts);
     }
 
     @Override
     public Stream<String> from(Collection<String> collection) {
-        return Stream.empty();
+        return collection.stream();
     }
 
     @Override
     public IntStream from(String s) {
-        return IntStream.empty();
+        return s.chars();
     }
 
     @Override
     public IntStream infiniteAlternating() {
-        return IntStream.empty();
+        final long limit = 19;
+        return IntStream.iterate(1, i -> -1 * i).limit(limit);
     }
 
     @Override
     public IntStream infiniteRandomInts(Random rnd) {
-        return IntStream.empty();
+        final int seed = 42;
+        final long limit = 19;
+
+        return new Random(seed).ints().limit(limit);
     }
 
     @Override
     public Stream<String> linesFromPoemTxtFile() throws IOException {
-        return Stream.empty();
+        return Files.lines(Paths.get("poem.txt"));
     }
 }
--- src/main/java/com/speedment/example/solution/Unit2MyIntermediate.java
+++ src/main/java/com/speedment/example/solution/Unit2MyIntermediate.java
@@ -2,6 +2,7 @@ package com.speedment.example.solution;
 
 import com.speedment.example.unit.Unit2Intermediate;
 
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -9,31 +10,32 @@ public final class Unit2MyIntermediate implements Unit2Intermediate {
 
     @Override
     public Stream<String> wordsLongerThanThreeChars(Stream<String> stream) {
-        return Stream.empty();
+        return stream.filter(s-> s.length() > 3);
     }
 
     @Override
     public Stream<String> firstTwoWordsLongerThanThreeChars(Stream<String> stream) {
-        return Stream.empty();
+        return stream.filter(s -> s.length() > 3).limit(2);
     }
 
     @Override
     public Stream<String> firstDistinctTwoWordsLongerThanThreeCharsInAlphabeticOrder(Stream<String> stream) {
-        return Stream.empty();
+        return stream.filter(s -> s.length() > 3).distinct().limit(2).sorted();
     }
 
     @Override
     public IntStream lengthOfWords(Stream<String> stream) {
-        return IntStream.empty();
+        return stream.mapToInt(String::length);
     }
 
     @Override
     public IntStream increasingSawtooth() {
-        return IntStream.empty();
+        final long limit = 15;
+        return IntStream.range(0, Integer.MAX_VALUE).flatMap(i -> IntStream.range(0, i)).limit(limit);
     }
 
     @Override
     public Stream<String> strings(Stream<Object> stream) {
-        return Stream.empty();
+        return stream.filter(String.class::isInstance).map(String.class::cast);
     }
 }
